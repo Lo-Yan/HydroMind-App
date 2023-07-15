ae62aa0c97e990b3ba95e1f01709237dde0123cb
Lo-Yan <loyanmax@gmail.com>
2023-07-16 00:08:18 +0800

    Merged #5
    Goal screen done, ready to link up with home screen

--- lib/screens/goals_screen.dart
+++ lib/screens/goals_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'home_screen.dart';
 
 class GoalsScreen extends StatelessWidget {
   const GoalsScreen({Key? key});
@@ -10,9 +10,104 @@ class GoalsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Goals Screen'),
       ),
-      body: Center(
-        child: const Text('This is the goals screen.'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildGoalBox(
+              'assets/images/goal1.png',
+              'Novice',
+              context,
+            ),
+            _buildGoalBox(
+              'assets/images/goal2.png',
+              'Seasoned',
+              context,
+            ),
+            _buildGoalBox(
+              'assets/images/goal3.png',
+              'Elite',
+              context,
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildGoalBox(String imagePath, String description, BuildContext context) {
+    return InkWell(
+      onTap: () {
+        _showConfirmationDialog(context);
+      },
+      child: Container(
+        width: double.infinity,
+        height: 200,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 150,
+                  child: Image.asset(imagePath),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    description,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 30),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showConfirmationDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Confirmation'),
+          content: const Text('Are you sure you want to proceed?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomeScreen(),
+                  ),
+                );
+              },
+              child: const Text('Confirm'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+              },
+              child: const Text('Cancel'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
