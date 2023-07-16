5e5340543cbfd0c162bffec9139967525fd274bd
shuhuiii <97970820+shuhuiii@users.noreply.github.com>
2023-07-16 15:04:50 +0800

    add top nav bar for water usage

--- lib/screens/your_water_usage_screen.dart
+++ lib/screens/your_water_usage_screen.dart
@@ -1,19 +1,86 @@
 import 'package:flutter/material.dart';
 
+class YourWaterUsageScreen extends StatefulWidget {
+  const YourWaterUsageScreen({Key? key}) : super(key: key);
 
-import 'package:flutter/material.dart';
+  @override
+  _YourWaterUsageScreenState createState() => _YourWaterUsageScreenState();
+}
 
-class YourWaterUsageScreen extends StatelessWidget {
-  const YourWaterUsageScreen({Key? key});
+class _YourWaterUsageScreenState extends State<YourWaterUsageScreen> {
+  int _selectedIndex = 0;
+
+  void _onNavItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Water Usage Screen'),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(48),
+          child: Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    _onNavItemTapped(0);
+                  },
+                  child: Container(
+                    color: _selectedIndex == 0 ? Colors.blue : Colors.transparent,
+                    child: const Center(
+                      child: Text(
+                        'Weeks',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    _onNavItemTapped(1);
+                  },
+                  child: Container(
+                    color: _selectedIndex == 1 ? Colors.blue : Colors.transparent,
+                    child: const Center(
+                      child: Text(
+                        'Months',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Center(
-        child: const Text('This is the your water usage screen.'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Text(
+                _selectedIndex == 0 ? 'This is the weeks screen.' : 'This is the months screen.',
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
